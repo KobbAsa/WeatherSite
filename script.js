1a73a87711a51c3ed3a77215a91b090ec3229481
Antonio <antony.dovzhenko@gmail.com>
2023-05-04 19:27:48 +0300

    Added new variables and small changes

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const apiKey = '143a2fc2324eda5597969126343ac407';
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=Kyiv&units=metric&appid=${apiKey}`;
+let city = 'Kyiv';
+const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
 
 const currentCityName = document.querySelector('.current-city-name');
 const currentTemperature = document.querySelector('.current-temperature');
 const description = document.querySelector('.description');
+const searchBar = document.querySelector('.search-bar');
 
 async function getWeatherData(){
     const response = await fetch(apiUrl);
@@ -18,12 +20,12 @@ getWeatherData().then(data => {
     description.textContent = data.list[0].weather[0].description;
 }).catch(error => console.log(error));
 
-    function updateDateTime() {
-        const today = new Date();
-        const date = today.toLocaleDateString();
-        const time = today.toLocaleTimeString();
-        document.getElementById('date-time').innerHTML = `Date: ${date} <br> Time: ${time}`;
-    }
+function updateDateTime() {
+    const today = new Date();
+    const date = today.toLocaleDateString();
+    const time = today.toLocaleTimeString();
+    document.getElementById('date-time').innerHTML = `Date: ${date} <br> Time: ${time}`;
+}
 
-    updateDateTime();
-    setInterval(updateDateTime, 1000);
+updateDateTime();
+setInterval(updateDateTime, 1000);
