638f6060b269e1182dd471ca23a1282ae3d7b019
Antonio <antony.dovzhenko@gmail.com>
2023-05-05 11:14:25 +0300

    Added functionality for searching by city and updated current weather block

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
 const apiKey = '143a2fc2324eda5597969126343ac407';
 let city = 'Kyiv';
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
 
 const currentCityName = document.querySelector('.current-city-name');
 const currentTemperature = document.querySelector('.current-temperature');
 const description = document.querySelector('.description');
 const searchBar = document.querySelector('.search-bar');
-const searchButton = document.getElementById('.search-btn');
+const searchButton = document.getElementById('search-btn');
 
-async function getWeatherData(){
+async function getWeatherData(city){
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
     return data;
 }
 
-getWeatherData().then(data => {
-    console.log(data)
-    currentCityName.textContent = data.city.name;
-    currentTemperature.textContent = Math.round(data.list[0].main.temp) + '°C';
-    description.textContent = data.list[0].weather[0].description;
-}).catch(error => console.log(error));
+function updateWeatherData(city) {
+    getWeatherData(city).then(data => {
+        console.log(data)
+        currentCityName.textContent = data.city.name;
+        currentTemperature.textContent = Math.round(data.list[0].main.temp) + '°C';
+        description.textContent = data.list[0].weather[0].description;
+    }).catch(error => console.log(error));
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-    const searchButton = document.getElementById('search-btn');
-    searchButton.addEventListener('click', () => {
-        console.log('button works properly')
-    });
+updateWeatherData(city);
+
+searchButton.addEventListener('click', () => {
+    const updatedCity = searchBar.value.trim();
+    if(updatedCity !== ``){
+        city = updatedCity;
+        updateWeatherData(city);
+    }
 });
 
 function updateDateTime() {
