08318dd74f69f3931f81f79f5bb139d5bc4fd7c5
Antonio <antony.dovzhenko@gmail.com>
2023-06-07 16:15:51 +0300

    Made code prettier via Prettier.io :)

--- script.js
+++ script.js
@@ -1,67 +1,71 @@
-const apiKey = '143a2fc2324eda5597969126343ac407';
-let city = 'Kyiv';
+const apiKey = "143a2fc2324eda5597969126343ac407";
+let city = "Kyiv";
 
 // basic search
-const currentCityName = document.querySelector('.current-city-name');
-const currentTemperature = document.querySelector('.current-temperature');
-const description = document.querySelector('.description');
-const searchBar = document.querySelector('.search-bar');
-const searchButton = document.getElementById('search-btn');
-const coord = document.getElementById('coordinates');
+const currentCityName = document.querySelector(".current-city-name");
+const currentTemperature = document.querySelector(".current-temperature");
+const description = document.querySelector(".description");
+const searchBar = document.querySelector(".search-bar");
+const searchButton = document.getElementById("search-btn");
+const coord = document.getElementById("coordinates");
 
 // current info block
-const wind = document.querySelector('.weather-value-wind');
-const humidity = document.querySelector('.weather-value-humidity');
-const pressure = document.querySelector('.weather-value-pressure');
-const sunrise = document.querySelector('.weather-value-sunrise');
-const sunset = document.querySelector('.weather-value-sunset');
-const airQuality = document.querySelector('.weather-value-airQuality')
+const wind = document.querySelector(".weather-value-wind");
+const humidity = document.querySelector(".weather-value-humidity");
+const pressure = document.querySelector(".weather-value-pressure");
+const sunrise = document.querySelector(".weather-value-sunrise");
+const sunset = document.querySelector(".weather-value-sunset");
+const airQuality = document.querySelector(".weather-value-airQuality");
 
 // five-day forecast block
-const forecastDates = document.querySelectorAll('.weather-date');
-const forecastTemp = document.querySelectorAll('.weather-temperature');
-const forecastDescription = document.querySelectorAll('.weather-additional-info');
+const forecastDates = document.querySelectorAll(".weather-date");
+const forecastTemp = document.querySelectorAll(".weather-temperature");
+const forecastDescription = document.querySelectorAll(
+    ".weather-additional-info"
+);
 
-const lastForecastDesc = document.querySelector('.last-weather-additional-info');
-const lastForecastDate = document.querySelector('.last-weather-date');
-const lastForecastTemp = document.querySelector('.last-weather-temperature');
+const lastForecastDesc = document.querySelector(
+    ".last-weather-additional-info"
+);
+const lastForecastDate = document.querySelector(".last-weather-date");
+const lastForecastTemp = document.querySelector(".last-weather-temperature");
 
 // for change format button
-const timeFormatBtn = document.getElementById('time-format-btn');
-const timeFormatDisplay = document.getElementById('date-time');
-const dateFormatBtn = document.getElementById('date-format-btn');
-const toggleTempBtn = document.getElementById('temp-toggle-btn');
+const timeFormatBtn = document.getElementById("time-format-btn");
+const timeFormatDisplay = document.getElementById("date-time");
+const dateFormatBtn = document.getElementById("date-format-btn");
+const toggleTempBtn = document.getElementById("temp-toggle-btn");
 
 // format variables
-let timeFormat = 'en-GB';
-let dateFormat = 'en-GB';
-let temperatureUnit = 'metric';
+let timeFormat = "en-GB";
+let dateFormat = "en-GB";
+let temperatureUnit = "metric";
 
 // fav list
-const favoritesList = document.getElementById('favorites-list');
-const addToFavsBtn = document.getElementById('add-to-favs-btn');
-const clearFavsBtn = document.getElementById('clear-favorites-btn');
+const favoritesList = document.getElementById("favorites-list");
+const addToFavsBtn = document.getElementById("add-to-favs-btn");
+const clearFavsBtn = document.getElementById("clear-favorites-btn");
 
 let selectedCity = city;
 
-function getCityName(input){
+function getCityName(input) {
     let city = input.value.trim();
     city = city.charAt(0).toUpperCase() + city.slice(1);
     return city;
 }
-searchButton.addEventListener('click', () => {
+searchButton.addEventListener("click", () => {
     city = getCityName(searchBar);
-    if(city !== ``){
+    if (city !== ``) {
         updateCurrentWeather(city);
         updateForecast(city);
     }
-    searchBar.value = '';
+    searchBar.value = "";
 });
 
-searchBar.addEventListener('keydown', (event) => {
-    if(event.key === 'Enter'){
+searchBar.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
         city = getCityName(searchBar);
-        if(city !== ``){
+        if (city !== ``) {
             updateCurrentWeather(city);
             updateForecast(city);
         }
@@ -69,25 +73,24 @@ searchBar.addEventListener('keydown', (event) => {
 });
 
 function toggleTimeFormat() {
-    timeFormat = timeFormat === 'en-GB' ? 'en-US' : 'en-GB';
+    timeFormat = timeFormat === "en-GB" ? "en-US" : "en-GB";
     updateDateTime();
     updateCurrentWeather(selectedCity);
     updateForecast(selectedCity);
 }
 
 function toggleDateFormat() {
-    dateFormat = dateFormat === 'en-GB' ? 'en-US' : 'en-GB';
+    dateFormat = dateFormat === "en-GB" ? "en-US" : "en-GB";
     updateDateTime();
     updateCurrentWeather(selectedCity);
     updateForecast(selectedCity);
 }
 
 function toggleTemperatureUnit() {
-    temperatureUnit = temperatureUnit === 'metric' ? 'imperial' : 'metric';
+    temperatureUnit = temperatureUnit === "metric" ? "imperial" : "metric";
     updateCurrentWeather(selectedCity);
     updateForecast(selectedCity);
 }
-
 
 function updateDateTime() {
     const today = new Date();
@@ -102,19 +105,19 @@ function updateDateTime() {
 updateDateTime();
 setInterval(updateDateTime, 1000);
 
-timeFormatBtn.addEventListener('click', toggleTimeFormat);
-dateFormatBtn.addEventListener('click', toggleDateFormat);
-toggleTempBtn.addEventListener('click', toggleTemperatureUnit);
+timeFormatBtn.addEventListener("click", toggleTimeFormat);
+dateFormatBtn.addEventListener("click", toggleDateFormat);
+toggleTempBtn.addEventListener("click", toggleTemperatureUnit);
 
-function addZeros(value){
-    return value.toString().padStart(2, '0');
+function addZeros(value) {
+    return value.toString().padStart(2, "0");
 }
 
-async function getWeatherData(city){
+async function getWeatherData(city) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${temperatureUnit}&appid=${apiKey}`;
     const response = await fetch(apiUrl);
-    if(!response.ok){
-        throw new Error(`HTTP error! ${response.status}`)
+    if (!response.ok) {
+        throw new Error(`HTTP error! ${response.status}`);
     }
     const data = await response.json();
     return data;
@@ -123,32 +126,38 @@ async function getWeatherData(city){
 async function fetchWeatherData(city) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${temperatureUnit}&appid=${apiKey}`;
     const response = await fetch(apiUrl);
-    if(!response.ok){
-        throw new Error(`HTTP error! ${response.status}`)
+    if (!response.ok) {
+        throw new Error(`HTTP error! ${response.status}`);
     }
     const data = await response.json();
     return data;
 }
 
+
 function displayWeatherData(data, city) {
     selectedCity = city;
-    const temperatureSymbol = temperatureUnit === 'metric' ? '°C' : '°F';
+    const temperatureSymbol = temperatureUnit === "metric" ? "°C" : "°F";
 
     const lat = data.city.coord.lat;
     const lon = data.city.coord.lon;
 
     coord.innerHTML = `Latitude: ${lat}<br>Longitude: ${lon}`;
     currentCityName.textContent = `${data.city.name}, ${data.city.country}`;
-    currentTemperature.textContent = Math.round(data.list[0].main.temp) + temperatureSymbol;
+    currentTemperature.textContent =
+        Math.round(data.list[0].main.temp) + temperatureSymbol;
     description.textContent = data.list[0].weather[0].description;
-    wind.textContent = data.list[0].wind.speed + ' m/s';
-    humidity.textContent = data.list[0].main.humidity + ' %';
-    pressure.textContent = data.list[0].main.pressure + ' hPa';
+    wind.textContent = data.list[0].wind.speed + " m/s";
+    humidity.textContent = data.list[0].main.humidity + " %";
+    pressure.textContent = data.list[0].main.pressure + " hPa";
 
     const timezoneOffset = new Date().getTimezoneOffset();
 
-    const sunriseTime = new Date((data.city.sunrise + timezoneOffset * 60 + data.city.timezone) * 1000);
-    const sunsetTime = new Date((data.city.sunset + timezoneOffset * 60 + data.city.timezone) * 1000);
+    const sunriseTime = new Date(
+        (data.city.sunrise + timezoneOffset * 60 + data.city.timezone) * 1000
+    );
+    const sunsetTime = new Date(
+        (data.city.sunset + timezoneOffset * 60 + data.city.timezone) * 1000
+    );
 
     sunrise.textContent = sunriseTime.toLocaleTimeString(timeFormat);
     sunset.textContent = sunsetTime.toLocaleTimeString(timeFormat);
@@ -157,13 +166,15 @@ function displayWeatherData(data, city) {
 }
 function handleFetchError(error) {
     console.log(error);
-    alert(`${error.name}: ${city} is not valid city name. Please enter city name again! `)
-    city = '';
+    alert(
+        `${error.name}: ${city} is not valid city name. Please enter city name again! `
+    );
+    city = "";
 }
 
 function updateCurrentWeather(city) {
     fetchWeatherData(city)
-        .then(data => displayWeatherData(data, city))
+        .then((data) => displayWeatherData(data, city))
         .catch(handleFetchError);
 }
 
@@ -171,57 +182,72 @@ updateCurrentWeather(city);
 
 function updateForecast(city) {
     selectedCity = city;
-    const temperatureSymbol = temperatureUnit === 'metric' ? '°C' : '°F';
-    getWeatherData(city).then(data => {
-        const filteredData = data.list.filter(item => item.dt_txt.includes('12:00:00'));
+    const temperatureSymbol = temperatureUnit === "metric" ? "°C" : "°F";
+    getWeatherData(city).then((data) => {
+        const filteredData = data.list.filter((item) =>
+            item.dt_txt.includes("12:00:00")
+        );
         console.log(filteredData);
 
         const now = new Date();
         let currentHour = now.getHours();
         let startIndex = 0;
 
-        if(currentHour < 15) {
-            startIndex = 1
-            lastForecastDate.textContent = new Date(data.list[39].dt * 1000 + data.city.timezone * 1000).toLocaleDateString(dateFormat)+ ` (${data.list[39].dt_txt.split(' ')[1]})`;
-            lastForecastTemp.textContent = Math.round(data.list[39].main.temp) + temperatureSymbol;
-            document.getElementById('icon4').src = `https://openweathermap.org/img/w/${data.list[39].weather[0].icon}.png`;
+        if (currentHour < 15) {
+            startIndex = 1;
+            lastForecastDate.textContent =
+                new Date(
+                    data.list[39].dt * 1000 + data.city.timezone * 1000
+                ).toLocaleDateString(dateFormat) +
+                ` (${data.list[39].dt_txt.split(" ")[1]})`;
+            lastForecastTemp.textContent =
+                Math.round(data.list[39].main.temp) + temperatureSymbol;
+            document.getElementById(
+                "icon4"
+            ).src = `https://openweathermap.org/img/w/${data.list[39].weather[0].icon}.png`;
             lastForecastDesc.textContent = data.list[39].weather[0].description;
-        }
-        else {
-            lastForecastDate.textContent = new Date(filteredData[4].dt * 1000 + data.city.timezone * 1000).toLocaleDateString(dateFormat);
-            lastForecastTemp.textContent = Math.round(filteredData[4].main.temp) + temperatureSymbol;
+        } else {
+            lastForecastDate.textContent = new Date(
+                filteredData[4].dt * 1000 + data.city.timezone * 1000
+            ).toLocaleDateString(dateFormat);
+            lastForecastTemp.textContent =
+                Math.round(filteredData[4].main.temp) + temperatureSymbol;
             lastForecastDesc.textContent = filteredData[4].weather[0].description;
         }
 
-        filteredData.slice(startIndex).forEach((item, index) =>{
-            const forecastDate = new Date(item.dt * 1000 + data.city.timezone * 1000).toLocaleDateString(dateFormat);
+        filteredData.slice(startIndex).forEach((item, index) => {
+            const forecastDate = new Date(
+                item.dt * 1000 + data.city.timezone * 1000
+            ).toLocaleDateString(dateFormat);
             const weatherCode = filteredData[index].weather[0].icon;
             const weatherIconUrl = `https://openweathermap.org/img/w/${weatherCode}.png`;
-            const weatherIcon = document.getElementById('icon' + index);
+            const weatherIcon = document.getElementById("icon" + index);
 
             weatherIcon.src = weatherIconUrl;
 
             forecastDates[index].textContent = forecastDate;
-            forecastTemp[index].textContent = Math.round(item.main.temp) + temperatureSymbol;
-            forecastDescription[index].textContent = filteredData[index].weather[0].description;
-        })
-    })
+            forecastTemp[index].textContent =
+                Math.round(item.main.temp) + temperatureSymbol;
+            forecastDescription[index].textContent =
+                filteredData[index].weather[0].description;
+        });
+    });
 }
 
 updateForecast(city);
 
-let favorites = JSON.parse(localStorage.getItem('favoriteCities')) || [];
+let favorites = JSON.parse(localStorage.getItem("favoriteCities")) || [];
 
-function saveFavorites(){
-    localStorage.setItem('favoriteCities', JSON.stringify(favorites));
+function saveFavorites() {
+    localStorage.setItem("favoriteCities", JSON.stringify(favorites));
 }
 
-function displayFavorites(){
-    favoritesList.innerHTML = '';
+function displayFavorites() {
+    favoritesList.innerHTML = "";
     favorites.forEach((city) => {
-        const listItem = document.createElement('li');
+        const listItem = document.createElement("li");
         listItem.textContent = city;
-        listItem.addEventListener('click', () => {
+        listItem.addEventListener("click", () => {
             selectedCity = listItem.textContent;
             updateCurrentWeather(selectedCity);
             updateForecast(selectedCity);
@@ -230,58 +256,59 @@ function displayFavorites(){
     });
 }
 
-async function addToFavorites(){
+async function addToFavorites() {
     try {
         await fetchWeatherData(city);
-        if(!favorites.includes(city)){
+        if (!favorites.includes(city)) {
             favorites.push(city);
             saveFavorites();
             displayFavorites();
         }
     } catch (error) {
-        console.error('Cannot add non-existing city!');
+        console.error("Cannot add non-existing city!");
     }
 }
 
-addToFavsBtn.addEventListener('click', addToFavorites);
+addToFavsBtn.addEventListener("click", addToFavorites);
 
-clearFavsBtn.addEventListener('click', () => {
+clearFavsBtn.addEventListener("click", () => {
     favorites = [];
     saveFavorites();
     displayFavorites();
-})
+});
 
 displayFavorites();
 
 function toggleFeedback() {
-    const feedbackForm = document.querySelector('.feedback-form');
-    feedbackForm.style.display = feedbackForm.style.display === 'none' ? 'block' : 'none';
+    const feedbackForm = document.querySelector(".feedback-form");
+    feedbackForm.style.display =
+        feedbackForm.style.display === "none" ? "block" : "none";
 }
 
-const userComment = document.getElementById('comment-input');
+const userComment = document.getElementById("comment-input");
 function saveFeedback(feedback) {
-    let feedbacks = JSON.parse(localStorage.getItem('feedbacks')) || [];
+    let feedbacks = JSON.parse(localStorage.getItem("feedbacks")) || [];
     feedbacks.push(feedback);
-    localStorage.setItem('feedbacks', JSON.stringify(feedbacks));
+    localStorage.setItem("feedbacks", JSON.stringify(feedbacks));
     return feedbacks;
 }
 
 function logFeedbacks(feedbacks) {
     feedbacks.forEach((feedback, index) => {
-        console.log(`Feedback ${index+1}:`);
+        console.log(`Feedback ${index + 1}:`);
         console.log(`Name: ${feedback.name}`);
         console.log(`Comment: ${feedback.comment}`);
     });
 }
 
-function sendFeedback(){
-    const userName = document.getElementById('name-input');
+function sendFeedback() {
+    const userName = document.getElementById("name-input");
 
     const name = userName.value.trim();
     const comment = userComment.value.trim();
 
-    if(name === '' || comment === ''){
-        alert('If you want to send feedback please fill all fields!');
+    if (name === "" || comment === "") {
+        alert("If you want to send feedback please fill all fields!");
         return;
     }
 
@@ -294,14 +321,14 @@ function sendFeedback(){
 
     logFeedbacks(feedbacks);
 
-    userName.value = '';
-    userComment.value = '';
+    userName.value = "";
+    userComment.value = "";
 
-    alert('Thank you for your feedback!')
+    alert("Thank you for your feedback!");
 }
 
-userComment.addEventListener('keydown', (event) => {
-    if(event.key === 'Enter'){
+userComment.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
         sendFeedback();
     }
 });
@@ -309,8 +336,8 @@ userComment.addEventListener('keydown', (event) => {
 async function getAirQualityData(lat, lon) {
     const apiUrl = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`;
     const response = await fetch(apiUrl);
-    if(!response.ok){
-        throw new Error(`HTTP error! ${response.status}`)
+    if (!response.ok) {
+        throw new Error(`HTTP error! ${response.status}`);
     }
     const data = await response.json();
     console.log(data);
@@ -318,19 +345,21 @@ async function getAirQualityData(lat, lon) {
 }
 
 const airQualityLevels = {
-    1: {description: 'Good', color: "#00FF00"},
-    2: {description: 'Fair', color: "#CCCC00"},
-    3: {description: 'Moderate', color: "#CC6600"},
-    4: {description: 'Poor', color: "#990000"},
-    5: {description: 'Very Poor', color: "#660000"}
-}
+    1: { description: "Good", color: "#00FF00" },
+    2: { description: "Fair", color: "#CCCC00" },
+    3: { description: "Moderate", color: "#CC6600" },
+    4: { description: "Poor", color: "#990000" },
+    5: { description: "Very Poor", color: "#660000" },
+};
 
-function updateAirQuality(lat, lon){
-    getAirQualityData(lat, lon).then(data => {
-        let aqi = data.list[0].main.aqi;
-        airQuality.textContent = airQualityLevels[aqi].description;
-        airQuality.style.color = airQualityLevels[aqi].color;
-    }).catch(error =>{
-        console.log(error);
-    })
+function updateAirQuality(lat, lon) {
+    getAirQualityData(lat, lon)
+        .then((data) => {
+            let aqi = data.list[0].main.aqi;
+            airQuality.textContent = airQualityLevels[aqi].description;
+            airQuality.style.color = airQualityLevels[aqi].color;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
